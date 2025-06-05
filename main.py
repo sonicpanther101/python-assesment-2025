1e8d9f1dd89154d2712b7631e31367a241d1e2d6
sonicpanther101 <sonicpanther101@gmail.com>
2025-06-05 12:46:19 +1200

    starting work on grid system https://customtkinter.tomschimansky.com/tutorial/grid-system

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import customtkinter as ctk
 from PIL import Image
 
+class item:
+    def __init__(self, name, price, image):
+        self.name = name
+        self.price = price
+        self.image = "images/" + image
+
 global cart
 cart = []
 
@@ -13,12 +19,6 @@ ITEMS = [
     item("soft drink", 1.5, "soft drink.jpg"),
 ]
 
-class item:
-    def __init__(self, name, price, image):
-        self.name = name
-        self.price = price
-        self.image = "images/" + image
-
 def addToCart(item):
     cart.append(item)
 
@@ -30,36 +30,38 @@ app.geometry("900x600")
 app.title("Reseraunt App")
 
 class button:
-    def __init__(self, text, command):
+    def __init__(self, text, command, width):
         self.button = ctk.CTkButton(app, text=text, command=command)
         self.button.pack()
 
 class image:
-    def __init__(self, path):
-        self.image = 
-        self.label = 
+    def __init__(self, path, text):
+        self.image = ctk.CTkImage(Image.open(path), size=(100, 100))
+        self.label = ctk.CTkLabel(app, image=self.image, text="", columnspan=2)
+        self.label.pack()
+        self.label = ctk.CTkLabel(app, text=text)
         self.label.pack()
 
 # Hamburger
-image(ITEMS[0].image)
-button("+", lambda: addToCart(ITEMS[0]))
-button("-", lambda: removeFromCart(ITEMS[0]))
+image(ITEMS[0].image, ITEMS[0].name)
+button("+", lambda: addToCart(ITEMS[0]), 1)
+button("-", lambda: removeFromCart(ITEMS[0]), 1)
 
 # Cheeseburger
-button("+", lambda: addToCart(ITEMS[1]))
-button("-", lambda: removeFromCart(ITEMS[1]))
+button("+", lambda: addToCart(ITEMS[1]), 1)
+button("-", lambda: removeFromCart(ITEMS[1]), 1)
 
 # Veggie Wrap
-button("+", lambda: addToCart(ITEMS[2]))
-button("-", lambda: removeFromCart(ITEMS[2]))
+button("+", lambda: addToCart(ITEMS[2]), 1)
+button("-", lambda: removeFromCart(ITEMS[2]), 1)
 
 # French Fries
-button("+", lambda: addToCart(ITEMS[3]))
-button("-", lambda: removeFromCart(ITEMS[3]))
+button("+", lambda: addToCart(ITEMS[3]), 1)
+button("-", lambda: removeFromCart(ITEMS[3]), 1)
 
 # Soft Drink
-button("+", lambda: addToCart(ITEMS[4]))
-button("-", lambda: removeFromCart(ITEMS[4]))
+button("+", lambda: addToCart(ITEMS[4]), 1)
+button("-", lambda: removeFromCart(ITEMS[4]), 1)
 
 button("Cart", lambda: print(cart))
 
